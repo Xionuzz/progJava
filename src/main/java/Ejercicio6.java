00f65c9149bfc556ab3a999c90636a3a3dbf0b6f
Izan <izanfarmonde@hotmail.es>
2020-02-26 16:50:40 +0100

    nuevos ejercicios

--- src/main/java/Ejercicio6.java
+++ src/main/java/Ejercicio6.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class Ejercicio6 {
+    public Ejercicio6(){
+
+        Scanner scan = new Scanner(System.in);
+
+        char letra;
+        int veces;
+        int lineas;
+
+        System.out.println("Introduce el caracter que deseas imprimir: ");
+        letra = scan.nextLine().charAt(0);
+        System.out.println("Introduce el número de veces que deseas impromirlo: ");
+        veces = scan.nextInt();
+        System.out.println("Introduce las filas: ");
+        lineas = scan.nextInt();
+
+        imprimir(letra, veces, lineas);
+    }
+    public static void imprimir(char letra, int veces, int lineas){
+        for (int i = 0; i < lineas; i++) {
+            for (int j = 0; j < veces; j++) {
+                System.out.printf("%s", letra);
+            }
+            System.out.println("");
+        }
+    }
 }
